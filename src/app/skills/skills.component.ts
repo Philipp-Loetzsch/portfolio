2a01ed460d1d4f6454357c40b9e3c272e5c03d93
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-10-22 21:27:38 +0200

    esit style skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-
+  skills = [
+    {img:'html', skill:'HTML'},
+    {img:'css', skill:'CSS'},
+    {img:'javascript', skill:'JavaScript'},
+    {img:'materialdesign', skill:'Material Design'},
+    {img:'typescript', skill:'TypeScript'},
+    {img:'angular', skill:'Angular'},
+    {img:'firebase', skill:'Firebase'},
+    {img:'git', skill:'Git'},
+    {img:'api', skill:'Rest-Api'},
+    {img:'scrum', skill:'Scrum'},
+    {img:'growth', skill:'Growth mindset'},
+  ]
 }
