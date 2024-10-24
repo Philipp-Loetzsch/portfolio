a36c7fc3c66e57106c4f81a81693bb554640bb42
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-10-24 16:02:31 +0200

    animate marquee and edit contact

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,14 +9,25 @@ import { CommonModule } from '@angular/common';
   styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
+  lpLinks = [
+    {
+      jump: '#',
+      content: 'Check my work',
+    },
+    {
+      jump: '#',
+      content: 'Contact me',
+    },
+  ];
+
   socialMedias = [
     {
-      img:'mail',
-      link:'mailto:philipp.loetzsch@outlook.com'
+      img: 'mail',
+      link: 'mailto:philipp.loetzsch@outlook.com',
     },
-    { 
-      img: 'github', 
-      link: 'https://github.com/Philipp-Loetzsch' 
+    {
+      img: 'github',
+      link: 'https://github.com/Philipp-Loetzsch',
     },
     {
       img: 'linkedIn',
@@ -24,7 +35,46 @@ export class LandingPageComponent {
     },
   ];
 
-  startAnimation(){
-    
+  texts = ['Available for remote work' , 'Frontend Developer', 'Based in Chemnitz', 'open to work', 'Available for remote work' , 'Frontend Developer', 'Based in Chemnitz', 'open to work']
+  screenWidth: number = window.innerWidth;
+
+
+  ngOnInit() {
+
+    this.texts.forEach((text, index) => {
+      this.startAnimation(text, this.screenWidth, -this.screenWidth);
+    });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = event.target.innerWidth;
+    this.texts.forEach((text) => this.stopAnimation(text));
+    this.texts.forEach((text, index) => {
+      this.startAnimation(text, this.screenWidth, -this.screenWidth);
+    });
+  }
+
+  marqueeLinkX: { [key: string]: number } = {};
+  animations: { [key: string]: any } = {};
+
+  startAnimation(content: string, maxXLeft: number, xRight: number) {
+    this.stopAnimation(content);
+    this.marqueeLinkX[content] = this.marqueeLinkX[content] || 0;
+
+    this.animations[content] = setInterval(() => {
+      if (this.marqueeLinkX[content] > maxXLeft) {
+        this.marqueeLinkX[content] = xRight;
+      }
+      this.marqueeLinkX[content]++;
+    }, 1000 / 40);
+  }
+
+  stopAnimation(content: string) {
+    if (this.animations[content]) {
+      clearInterval(this.animations[content]);
+      delete this.animations[content];
+      this.marqueeLinkX[content] = 0;
+    }
   }
 }
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,12 +3,13 @@ import { LandingPageComponent } from "../landing-page/landing-page.component";
 import { AboutMeComponent } from "../about-me/about-me.component";
 import { SkillsComponent } from "../skills/skills.component";
 import { ProjectsComponent } from "../projects/projects.component";
+import { ContactComponent } from "../contact/contact.component";
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, ProjectsComponent],
+  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
