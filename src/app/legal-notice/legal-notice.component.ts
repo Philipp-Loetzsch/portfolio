a4b4aae7f3225e1a99d9c8559d13ef0bf1459431
Philipp Lötszch <philipp.loetzsch@outlook.com>
2024-11-06 20:16:15 +0100

    generate imprint

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { TranslateService, TranslateDirective, TranslateModule, TranslatePipe } from '@codeandweb/ngx-translate';
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [],
+  imports: [TranslateDirective, TranslateModule, TranslatePipe],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
 export class LegalNoticeComponent {
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+}
 
 }
