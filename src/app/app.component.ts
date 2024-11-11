fbdaf86bf9327fdb348fa6853b75710907b6b5cb
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-11-11 23:10:28 +0100

    fix navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,19 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
-import { LandingPageComponent } from "./landing-page/landing-page.component";
-import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from "./shared/components/footer/footer.component";
-import { TranslateService, TranslateDirective, TranslatePipe, TranslateModule } from '@codeandweb/ngx-translate';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LandingPageComponent, MainContentComponent, FooterComponent, TranslatePipe, TranslateDirective, TranslateModule, CommonModule],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   screenWidth = window.innerWidth
   title = 'portfolio';
-    constructor(private translate: TranslateService) {
-        this.translate.addLangs(['de', 'en']);
-        this.translate.setDefaultLang('de');
-        this.translate.use('de');
-    }
-
     mouseX: number = 0;
     mouseY: number = 0;
   
