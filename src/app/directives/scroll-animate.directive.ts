f4988763033736e645f75a027ba0f500f053faf8
Philipp Lötzsch <philipp.loetzsch@outlook.com>
2024-11-13 19:34:39 +0100

    animate elements

--- src/app/directives/scroll-animate.directive.ts
+++ src/app/directives/scroll-animate.directive.ts
@@ -1,36 +1,3 @@
-/* import { Directive, ElementRef, Renderer2, OnInit, OnDestroy, Input } from '@angular/core';
-
-@Directive({
-  selector: '[appScrollAnimate]',
-  standalone: true
-})
-export class ScrollAnimateDirective implements OnInit, OnDestroy {
-  @Input('appScrollAnimate') animationType: 'fade-in' | 'slide-in-right' | 'slide-in-left' = 'fade-in';
-
-  private observer!: IntersectionObserver;
-
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
-
-  ngOnInit() {
-    this.observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          this.renderer.addClass(this.el.nativeElement, this.animationType);
-          this.observer.unobserve(entry.target); 
-        }
-      });
-    });
-    this.observer.observe(this.el.nativeElement);
-  }
-
-  ngOnDestroy() {
-    if (this.observer) {
-      this.observer.disconnect();
-    }
-  }
-}
- */
-
 import { Directive, ElementRef, Renderer2, OnInit, OnDestroy, Input } from '@angular/core';
 
 @Directive({
@@ -45,21 +12,17 @@ export class ScrollAnimateDirective implements OnInit, OnDestroy {
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    // IntersectionObserver einrichten
     this.observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          // Klasse 'visible' und die angegebene Animationstyp-Klasse hinzufügen
           this.renderer.addClass(this.el.nativeElement, 'visible');
           if (this.animationType) {
             this.renderer.addClass(this.el.nativeElement, this.animationType);
           }
-          this.observer.unobserve(entry.target); // Stoppt die Beobachtung nach dem ersten Aufruf
+          this.observer.unobserve(entry.target);
         }
       });
     });
-
-    // Startet die Beobachtung des Elements
     this.observer.observe(this.el.nativeElement);
   }
 
@@ -69,3 +32,5 @@ export class ScrollAnimateDirective implements OnInit, OnDestroy {
     }
   }
 }
+ 
+
